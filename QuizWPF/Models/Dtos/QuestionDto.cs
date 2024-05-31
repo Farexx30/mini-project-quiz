5a13e1c0fdabd3038c29d1f94786434560e408d7
Łukasz Karwacki <lkarwacki3@interia.pl>
2024-05-31 19:31:41 +0200

    Big changes connected with quiz generation

---
+++ QuizWPF/Models/Dtos/QuestionDto.cs
@@ -0,0 +1,40 @@
+﻿using QuizWPF.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace QuizWPF.Models.Dtos
+{
+    public class QuestionDto
+    {
+        public int Id { get; set; }
+        public string Value { get; set; } = string.Empty;
+        public List<AnswerDto> Answers { get; set; } = [];
+
+        public static uint QuestionNumber { private get; set; } = 1;
+        public override string ToString()
+        {
+            return $"Pytanie {QuestionNumber++}";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            var questionToCompare = obj as QuestionDto ?? null;
+
+            if (questionToCompare is not null && Value == questionToCompare.Value)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+    }
+}
---
+++ QuizWPF/Services/QuizRepositoryService.cs
@@ -0,0 +1,21 @@
+﻿using QuizWPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizWPF.Services
+{
+    public interface IQuizRepositoryService
+    {
+        QuizDbContext DbContext { get; }
+    }
+
+    public class QuizRepositoryService(QuizDbContext dbContext) : IQuizRepositoryService
+    {
+        public QuizDbContext DbContext { get; } = dbContext;
+
+        //Tutaj beda metody z operacjami na bazie danych takie jak pobierz/zapisz/zaktualizuj etc.
+    }
+}
